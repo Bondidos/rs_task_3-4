d6aa2dfe39dfae2ae08bd4203a40c94efc218c57
Bondidos <gglbond@gmail.com>
2021-06-23 15:46:19 +0300

    progress: 1 subtask in progress

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,34 @@
 package subtask1
 
+import java.lang.StringBuilder
+
 class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        /*@Test
+        fun testConvertToString1() {
+            val result = converter.convertToStringFrom(arrayOf(2, -1, 78))
+            assertEquals("2x^2 - x + 78", result)
+        }*/
+        // (if y<2 nx) (if y = 0 n) else nx^y
+        val str = StringBuilder("")
+        for(i in numbers){
+            if((numbers.size - numbers.indexOf(i) - 1 ) == 1){
+                //   -/+ cases
+                if(i>0)
+                    str.append("${i}x ")
+                if(i<0)
+                    str.append("${i}x ")
+            }
+            if ((numbers.size - numbers.indexOf(i) - 1 ) == 0){
+                str.append("$i ")
+            }
+            if ((numbers.size - numbers.indexOf(i) - 1 ) > 1){
+                str.append("${i}x^${(numbers.size - numbers.indexOf(i) - 1 )} ")
+            }
+        }
+        return str.toString()
+        //throw NotImplementedError("Not implemented")
     }
 }
