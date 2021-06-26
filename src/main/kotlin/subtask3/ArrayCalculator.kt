acf7e170c12f6e89db44c724d26a2c5d97562a8e
Bondidos <gglbond@gmail.com>
2021-06-26 15:42:43 +0300

    progress: 3 subtask in work

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,25 @@
 package subtask3
 
 class ArrayCalculator {
-
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var result = 0
+        val items = mutableListOf<Int>()
+        val map = mutableMapOf<Int,List<Int>>()
+                          //<resultOfMult, coeffecients>
+        itemsFromArray.forEach { if (it.checkForNonIntItem()) items.add(it as Int) }
+
+        for(i in items.indices){
+            for(j in i until items.size){
+                if(i==j) continue
+                val buffer = (items[i] * items[j])
+                val coefficients = listOf(items[i],items[j])
+                map[buffer] = coefficients
+            }
+        }
+        items.forEach { println(it) }
+        map.forEach { println(it) }
+        return result
     }
 }
+fun Any.checkForNonIntItem() = this is Int
+
